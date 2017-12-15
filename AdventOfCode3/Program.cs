2cf8800a4ccbc67bb8cec7fcbcf0661713c7c34b
Michal <mm.krupinski@gmail.com>
2017-12-15 23:31:53 +0100

    Commit all

--- AdventOfCode3/Program.cs
+++ AdventOfCode3/Program.cs
@@ -10,6 +10,43 @@ namespace AdventOfCode3
     {
         static void Main(string[] args)
         {
+            int inputData = 325489;
+            //int inputData = 25;
+            Console.WriteLine("Input data = {0}", inputData);
+            // Determine "square" on which puzzle input is
+
+            int squareNumber = 0;
+            int rightBottomCorner = 1;
+
+            while (rightBottomCorner < inputData)
+            {
+                squareNumber++;
+                rightBottomCorner = ((2 * squareNumber) + 1) * ((2 * squareNumber) + 1);
+                
+            }
+            Console.WriteLine("Square number = {0}", squareNumber);
+            Console.WriteLine("Right bottom corner values is: {0}", rightBottomCorner);
+            int spiralDistance = rightBottomCorner - inputData;
+            Console.WriteLine("Distance on spiral from corner to input number: {0}", spiralDistance);
+            Console.ReadLine();
+
+            // Getting back to input data from the corner
+            Console.WriteLine("Spiral Distance / square number = {0}", spiralDistance / squareNumber);
+            Console.WriteLine("Spiral Distance mod square number = {0}", spiralDistance % squareNumber);
+
+            int halfSteps = 0;
+            if (spiralDistance/squareNumber % 2 == 0)
+            {
+                halfSteps = squareNumber - (spiralDistance % squareNumber);
+            }
+            else
+            {
+                halfSteps = spiralDistance % squareNumber;
+            }
+            Console.WriteLine("Half steps = {0}", halfSteps);
+            Console.WriteLine("Total steps = {0}", halfSteps + squareNumber);
+
+            Console.ReadLine();
         }
     }
 }
